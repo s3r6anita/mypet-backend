dede523080f9ecaabdbfbdbf705eb70b7f03f3c8
s3r6anita <xz200359@gmail.com>
2024-05-21 15:49:41 +0300

    change frequency in Procedure

--- src/main/kotlin/ru/mypet/data/db/daos/ProcedureDAOImpl.kt
+++ src/main/kotlin/ru/mypet/data/db/daos/ProcedureDAOImpl.kt
@@ -7,30 +7,17 @@ import ru.mypet.data.db.DatabaseFactory.dbQuery
 import ru.mypet.data.db.Pets
 import ru.mypet.data.db.Procedures
 import ru.mypet.data.db.UpdateProcedureParams
-import ru.mypet.models.Pet
 import ru.mypet.models.Procedure
 import ru.mypet.utils.PetDateTimeFormatter
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 class ProcedureDAOImpl : ProcedureDAO {
-    private fun resultRowToPet(row: ResultRow) = Pet(
-        id = row[Pets.id],
-        name = row[Pets.name],
-        kind = row[Pets.kind],
-        breed = row[Pets.breed],
-        sex = row[Pets.sex],
-        birthday = LocalDate.parse(row[Pets.birthday], PetDateTimeFormatter.date),
-        color = row[Pets.color],
-        coat = row[Pets.coat],
-        microchipNumber = row[Pets.microchipNumber],
-        owner = row[Pets.owner]
-    )
 
     private fun resultRowToProcedure(row: ResultRow) = Procedure(
         title = row[Procedures.title],
         isDone = row[Procedures.isDone],
         frequency = row[Procedures.frequency],
+        frequencyOption = row[Procedures.frequencyOption],
         dateDone = LocalDateTime.parse(row[Procedures.dateDone], PetDateTimeFormatter.dateTime),
         notes = row[Procedures.notes],
         reminder = row[Procedures.reminder]?.let { LocalDateTime.parse(it, PetDateTimeFormatter.dateTime) },
@@ -55,6 +42,7 @@ class ProcedureDAOImpl : ProcedureDAO {
             it[title] = params.title
             it[isDone] = params.isDone
             it[frequency] = params.frequency
+            it[frequencyOption] = params.frequencyOption
             it[dateDone] = params.dateDone
             it[notes] = params.notes
             it[reminder] = params.reminder
@@ -76,6 +64,7 @@ class ProcedureDAOImpl : ProcedureDAO {
             it[title] = params.title
             it[isDone] = params.isDone
             it[frequency] = params.frequency
+            it[frequencyOption] = params.frequencyOption
             it[dateDone] = params.dateDone.format(PetDateTimeFormatter.dateTime)
             it[notes] = params.notes
             it[reminder] = params.reminder?.format(PetDateTimeFormatter.dateTime)
